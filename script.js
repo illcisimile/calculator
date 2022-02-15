d0931f2f4e698de11d70bd20f7d3aac57a767587
Jarod Bernabe <jarodbernabe1100@gmail.com>
2022-02-15 17:45:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,84 @@
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const clearButton = document.getElementById("clearBtn");
+const deleteButton = document.getElementById("deleteBtn");
+const lastOperationScreen = document.getElementById("lastOperationScreen");
+const currentOperationScreen = document.getElementById(
+  "currentOperationScreen"
+);
+const equalsButton = document.getElementById("equalsBtn");
+
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteNumber);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === "0" || shouldResetScreen) {
+    resetScreen();
+  }
+  currentOperationScreen.textContent += number;
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) {
+    evaluate();
+  }
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  shouldResetScreen = true;
+}
+
+function clear() {
+  lastOperationScreen.textContent = "";
+  currentOperationScreen.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) {
+    return;
+  }
+  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
+    alert("You cannot divide by 0!");
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = operate(
+    currentOperation,
+    firstOperand,
+    secondOperand
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -15,7 +96,17 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  if ((operator = "+")) {
-    return add(a, b);
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      return divide(a, b);
   }
 }
