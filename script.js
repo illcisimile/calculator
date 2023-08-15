8de32bc83eed51078d43848d7a85c9b99afea901
Jarod Bernabe <jarodbernabe1100@gmail.com>
2023-08-15 21:23:30 +0800

    refactored

--- script.js
+++ script.js
@@ -1,129 +1,136 @@
-let firstOperand = "";
-let secondOperand = "";
+let firstOperand = '';
+let secondOperand = '';
 let currentOperation = null;
 let shouldResetScreen = false;
 
-const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]");
-const clearButton = document.getElementById("clearBtn");
-const deleteButton = document.getElementById("deleteBtn");
-const lastOperationScreen = document.getElementById("lastOperationScreen");
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const lastOperationScreen = document.getElementById('lastOperationScreen');
 const currentOperationScreen = document.getElementById(
-  "currentOperationScreen"
+  'currentOperationScreen'
 );
-const pointButton = document.getElementById("pointBtn");
-const equalsButton = document.getElementById("equalsBtn");
+const pointButton = document.getElementById('pointBtn');
+const equalsButton = document.getElementById('equalsBtn');
 
-clearButton.addEventListener("click", clear);
-deleteButton.addEventListener("click", deleteNumber);
-pointButton.addEventListener("click", appendPoint);
-equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+pointButton.addEventListener('click', appendPoint);
+equalsButton.addEventListener('click', evaluate);
 
 numberButtons.forEach((button) =>
-  button.addEventListener("click", () => appendNumber(button.textContent))
+  button.addEventListener('click', () => appendNumber(button.textContent))
 );
 
 operatorButtons.forEach((button) =>
-  button.addEventListener("click", () => setOperation(button.textContent))
+  button.addEventListener('click', () => setOperation(button.textContent))
 );
 
-function appendNumber(number) {
-  if (currentOperationScreen.textContent === "0" || shouldResetScreen) {
+const appendNumber = (number) => {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen) {
     resetScreen();
   }
-  currentOperationScreen.textContent += number;
-}
 
-function appendPoint() {
+  currentOperationScreen.textContent += number;
+};
+
+const appendPoint = () => {
   if (shouldResetScreen) {
     resetScreen();
   }
-  if (currentOperationScreen.textContent === "") {
-    currentOperationScreen.textContent = "0";
+
+  if (currentOperationScreen.textContent === '') {
+    currentOperationScreen.textContent = '0';
   }
-  if (currentOperationScreen.textContent.includes(".")) {
+
+  if (currentOperationScreen.textContent.includes('.')) {
     return;
   }
-  currentOperationScreen.textContent += ".";
-}
 
-function resetScreen() {
-  currentOperationScreen.textContent = "";
+  currentOperationScreen.textContent += '.';
+};
+
+const resetScreen = () => {
+  currentOperationScreen.textContent = '';
   shouldResetScreen = false;
-}
+};
 
-function setOperation(operator) {
+const setOperation = (operator) => {
   if (currentOperation !== null) {
     evaluate();
   }
+
   firstOperand = currentOperationScreen.textContent;
   currentOperation = operator;
   lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
   shouldResetScreen = true;
-}
+};
 
-function clear() {
-  lastOperationScreen.textContent = "";
-  currentOperationScreen.textContent = "0";
-  firstOperand = "";
-  secondOperand = "";
+const clear = () => {
+  lastOperationScreen.textContent = '';
+  currentOperationScreen.textContent = '0';
+  firstOperand = '';
+  secondOperand = '';
   currentOperation = null;
-}
+};
 
-function deleteNumber() {
+const deleteNumber = () => {
   currentOperationScreen.textContent = currentOperationScreen.textContent
     .toString()
     .slice(0, -1);
-}
+};
 
-function evaluate() {
+const evaluate = () => {
   if (currentOperation === null || shouldResetScreen) {
     return;
   }
-  if (currentOperation === "÷" && currentOperationScreen.textContent === "0") {
-    alert("You cannot divide by 0!");
+
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert('You cannot divide by 0!');
     return;
   }
+
   secondOperand = currentOperationScreen.textContent;
   currentOperationScreen.textContent = roundResult(
     operate(currentOperation, firstOperand, secondOperand)
   );
   lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
   currentOperation = null;
-}
+};
 
-function roundResult(number) {
+const roundResult = (number) => {
   return Math.round(number * 1000) / 1000;
-}
+};
 
-function add(a, b) {
+const add = (a, b) => {
   return a + b;
-}
+};
 
-function subtract(a, b) {
+const subtract = (a, b) => {
   return a - b;
-}
+};
 
-function multiply(a, b) {
+const multiply = (a, b) => {
   return a * b;
-}
+};
 
-function divide(a, b) {
+const divide = (a, b) => {
   return a / b;
-}
+};
 
-function operate(operator, a, b) {
+const operate = (operator, a, b) => {
   a = Number(a);
   b = Number(b);
 
   switch (operator) {
-    case "+":
+    case '+':
       return add(a, b);
-    case "-":
+    case '-':
       return subtract(a, b);
-    case "x":
+    case 'x':
       return multiply(a, b);
-    case "÷":
+    case '÷':
       return divide(a, b);
   }
-}
+};
